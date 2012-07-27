d6692697bc2e513bfe7c4759c11a09620a47319a
Tgys <tgys@tgys.com>
2012-07-27 14:06:19 +0200

    Added a config variabele for the MySQL port

--- controller/Plugins_SM/database.js
+++ controller/Plugins_SM/database.js
@@ -7,6 +7,7 @@ exports.Init = function(core) {
 	var config = require('../config_database.js');
 	exports.connection = mysql.createConnection({
 	  host     : config.Ip,
+	  port     : config.Port,
 	  user     : config.User,
 	  password : config.Password,
 	  database : config.Database,
--- controller/config_database.js
+++ controller/config_database.js
@@ -6,6 +6,8 @@ var config = {};
 
 // Almost always localhost, as you should run the database at the same system as the controller does.
 config.Ip		= 'localhost'
+// MySQL port, by default 3306
+config.Port 	= 3306
 // Username
 config.User		= 'root'
 // Password
